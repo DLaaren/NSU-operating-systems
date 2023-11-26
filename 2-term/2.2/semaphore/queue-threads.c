98c8e997dfb89cb86912b9b699c0b36b967264fd
DLaaren <dlaaren@yandex.ru>
2023-11-26 14:03:21 +0700

    done

--- 2-term/2.2/semaphore/queue-threads.c
+++ 2-term/2.2/semaphore/queue-threads.c
@@ -18,117 +18,113 @@
 
 int size = 1000000;
 
-sem_t sem_read;
 sem_t sem_write;
-sem_t sem_lock;
+sem_t sem_read;;
 
 void set_cpu(int n) {
-	int err;
-	cpu_set_t cpuset;
-	pthread_t tid = pthread_self();
+  int err;
+  cpu_set_t cpuset;
+  pthread_t tid = pthread_self();
 
-	CPU_ZERO(&cpuset); 		// зануляем сет
-	CPU_SET(n, &cpuset);	// сет ядер = n
+  CPU_ZERO(&cpuset);     // зануляем сет
+  CPU_SET(n, &cpuset);  // сет ядер = n
 
-	err = pthread_setaffinity_np(tid, sizeof(cpu_set_t), &cpuset); // определяет на каком ядре будет выполняться !системный! поток
-	if (err) {
-		printf("set_cpu: pthread_setaffinity failed for cpu %d\n", n);
-		return;
-	}
+  err = pthread_setaffinity_np(tid, sizeof(cpu_set_t), &cpuset); // определяет на каком ядре будет выполняться !системный! поток
+  if (err) {
+    printf("set_cpu: pthread_setaffinity failed for cpu %d\n", n);
+    return;
+  }
 
-	printf("set_cpu: set cpu %d\n", n);
+  printf("set_cpu: set cpu %d\n", n);
 }
 
 void *reader(void *arg) {
-	int expected = 0;
-	queue_t *q = (queue_t *)arg;
-	printf("reader [pid : %d; ppid : %d; tid : %d]\n", getpid(), getppid(), gettid());
+  int expected = 0;
+  queue_t *q = (queue_t *)arg;
+  printf("reader [pid : %d; ppid : %d; tid : %d]\n", getpid(), getppid(), gettid());
 
-	set_cpu(1);
+  set_cpu(1);
 
-	while (1) {
+  while (1) {
+    sem_wait(&sem_read);
 
-		sem_wait(&sem_read);
-		sem_wait(&sem_lock);
+        while (q->count) {
+            int val = -1;
+        int ok = queue_get(q, &val); // достаём значение с очереди
+        if (expected != val)
+          printf(RED"ERROR: get value is %d but expected  %d" NOCOLOR "\n", val, expected);
 
-		int val = -1;
-		int ok = queue_get(q, &val); // достаём значение с очереди
-		if (expected != val)
-			printf(RED"ERROR: get value is %d but expected  %d" NOCOLOR "\n", val, expected);
+            expected = val + 1;
+        }
 
-		expected = val + 1;
+    sem_post(&sem_write);
+  }
 
-		sem_post(&sem_lock);
-		sem_post(&sem_write);
-	}
-
-	return NULL;
+  return NULL;
 }
 
 void *writer(void *arg) {
-	int i = 0;
-	queue_t *q = (queue_t *)arg;
-	printf("writer [pid : %d; ppid : %d; tid : %d]\n", getpid(), getppid(), gettid());
+    int i = 0;
+    queue_t *q = (queue_t *)arg;
+    printf("writer [pid : %d; ppid : %d; tid : %d]\n", getpid(), getppid(), gettid());
 
-	set_cpu(2);
+    set_cpu(2);
 
-	while (1) {
+    while (1) {
+        sem_wait(&sem_write);
 
-		sem_wait(&sem_write);
-		sem_wait(&sem_lock);
+        while (q->count < q->max_count) {
+            // usleep(1);
+            queue_add(q, i); // добавляем значение в очередь
+            i++;
+        }
 
-		// usleep(1);
-		int ok = queue_add(q, i); // добавляем значение в очередь
-		i++;
+        sem_post(&sem_read);
+    }
 
-		sem_post(&sem_lock);
-		sem_post(&sem_read);
-
-	}
-
-	return NULL;
+    return NULL;
 }
 
 
 int main() {
-	pthread_t tid;
-	queue_t *q;
-	int err;
+  pthread_t tid;
+  queue_t *q;
+  int err;
 
-	printf("main [pid : %d; ppid : %d; tid : %d]\n", getpid(), getppid(), gettid());
+  printf("main [pid : %d; ppid : %d; tid : %d]\n", getpid(), getppid(), gettid());
 
-	q = queue_init(size);   // создаём очередь
-	
-	if (sem_init(&sem_read, 0, 0) || sem_init(&sem_write, 0, size) || sem_init(&sem_lock, 0, 1)) {
-		printf("ERROR: sem_init() in main()\n");
-		return -1;
-	}
+  q = queue_init(size);   // создаём очередь
+  
+  if (sem_init(&sem_read, 0, 0) || sem_init(&sem_write, 0, 1)) {
+    printf("ERROR: sem_init() in main()\n");
+    return -1;
+  }
 
-	err = pthread_create(&tid, NULL, reader, q);   // создаём поток на чтение, передаём нашу очередь
-	if (err) {
-		printf("main: pthread_create() failed: %s\n", strerror(err));
-		return -1;
-	}
+  err = pthread_create(&tid, NULL, reader, q);   // создаём поток на чтение, передаём нашу очередь
+  if (err) {
+    printf("main: pthread_create() failed: %s\n", strerror(err));
+    return -1;
+  }
 
-	sched_yield();  // планировщик потоков 
-					// sched_yield() causes the calling thread to relinquish the CPU.
-       				// The thread is moved to the end of the queue for its static
-       				// priority and a new thread gets to run.
+  sched_yield();  // планировщик потоков 
+          // sched_yield() causes the calling thread to relinquish the CPU.
+               // The thread is moved to the end of the queue for its static
+               // priority and a new thread gets to run.
 
-	err = pthread_create(&tid, NULL, writer, q);   // создаём поток на запись, передаём нашу очередь
-	if (err) {
-		printf("main: pthread_create() failed: %s\n", strerror(err));
-		return -1;
-	}
+  err = pthread_create(&tid, NULL, writer, q);   // создаём поток на запись, передаём нашу очередь
+  if (err) {
+    printf("main: pthread_create() failed: %s\n", strerror(err));
+    return -1;
+  }
 
-	if (sem_destroy(&sem_read) || sem_destroy(&sem_write) || sem_destroy(&sem_lock)) {
-		printf("ERROR: sem_destroy() in main()\n");
-		return -1;
-	}
+  if (sem_destroy(&sem_read) || sem_destroy(&sem_write)) {
+    printf("ERROR: sem_destroy() in main()\n");
+    return -1;
+  }
 
-	// TODO: join threads
+  // TODO: join threads
 
-	pthread_exit(NULL);
+  pthread_exit(NULL);
 
-	return 0;
+  return 0;
 }
